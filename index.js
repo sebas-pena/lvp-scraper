9fb4365cdba19fb546e6f2c988c9b0c53fc07e2d
Pablo Peña <insomnionebulous@gmail.com>
2022-06-28 05:07:52 -0300

    Update : getPlayer now return nickname, photo url, stats and most played champions

--- index.js
+++ index.js
@@ -12,8 +12,45 @@ const cheerio = require("cheerio")
 const getPlayer = async (playerName, league) => {
 	const url = `https://${league}.lvp.global/jugador/${playerName}`
 	const response = await fetch(url)
-	const data = await response.text()
-	return data
+	const res = await response.text()
+
+	const $ = cheerio.load(res)
+	const nickname = $(".player-nickname").text()
+	const photoUrl = $(".player-photo > img").attr("src")
+
+	let playerStatsEl = $(".ranking-stats > div")
+	let playerStats = []
+	playerStatsEl.each((i, el) => {
+		let title = $(".ranking-stat-lbl", el).text()
+		let value = $("> span", el).text()
+		playerStats.push({ title, value })
+	})
+
+	let mostUsedEl = $(".most-used-container > div")
+	let mostUsed = []
+	mostUsedEl.each((i, el) => {
+		let name = $(".most-used-name", el).text()
+		let timesPlayed = $("span:nth-child(3)", el).text().split(" ")[0]
+		let winRate = $("span:nth-child(4)", el).text()
+
+		let champ = {
+			name,
+			timesPlayed,
+			winRate,
+		}
+
+		if (league === "superliga") {
+			champ.kda = $("span:nth-child(5)", el).text()
+		}
+		mostUsed.push(champ)
+	})
+
+	return {
+		nickname,
+		photoUrl,
+		playerStats,
+		mostUsed,
+	}
 }
 
 const getTeam = async (teamName, league) => {
@@ -22,3 +59,5 @@ const getTeam = async (teamName, league) => {
 	const data = await response.text()
 	return data
 }
+
+getPlayer("whiteknight", "superliga").then((res) => console.log(res))

81b740502212efa34269f8f7b719f64b105412a6
Pablo Peña <gmlpubli00@gmail.com>
2022-06-30 16:10:51 -0300

    Update getTeam return squad information

--- getTeam.js
+++ getTeam.js
@@ -9,7 +9,6 @@ const getTeam = async (teamName, league) => {
 
   // MATCHES
   let matches = []
-
   $("#owl-round > div").each((i, el) => {
     let teams = $(".team", el)
     teams = {
@@ -27,7 +26,50 @@ const getTeam = async (teamName, league) => {
       result: played ? result : null,
     })
   })
-  const squad = []
+
+  // PLAYERS
+  const squads = []
+  $(".squad-player-container").map((i, el) => {
+    const nickname = $(".player-nickname", el).text()
+    const name = $(".player-name", el).text()
+    const rol = $(".player-position", el).text()
+    const playerImg = $(".player-image", el).attr("src")
+    let bestChampionsEl = $(".bottom-player-card .stats-container > img", el)
+    let bestChampions = []
+
+    let nationalityAcronym = $(".flag-image", el)
+      .attr("src")
+      .split("/")
+      .pop()
+      .split(".")
+      .shift()
+    const player = {
+      nickname,
+      name,
+      rol,
+      playerImg,
+      nationalityAcronym,
+    }
+
+    if (bestChampionsEl.length > 0) {
+      bestChampionsEl.each((i, el) => {
+        bestChampions.push({
+          name: $(el).attr("alt"),
+          imageUrl: $(el).attr("src"),
+        })
+      })
+      player.bestChampions = bestChampions
+    }
+
+    let seasonKda = $(".kda-container .stats-player", el).text()
+    seasonKda ? (player.seasonKda = seasonKda) : null
+    squads.push(player)
+  })
+
+  console.log(squads)
+  return {
+    matches,
+  }
 }
 
 module.exports = getTeam

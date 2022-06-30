ea1a68b273865c02579f58654b616e74bbc44185
Pablo Peña <gmlpubli00@gmail.com>
2022-06-30 15:10:58 -0300

    Update

--- getTeam.js
+++ getTeam.js
@@ -2,11 +2,21 @@ const fetch = require("node-fetch")
 const cheerio = require("cheerio")
 
 const getTeam = async (teamName, league) => {
-  console.log(`Getting team ${teamName} from ${league}`)
   const url = `https://${league}.lvp.global/equipo/${teamName}`
   const response = await fetch(url)
-  const data = await response.text()
+  const res = await response.text()
   const $ = cheerio.load(res)
+
+  // MATCHES
+  $("#owl-round > div").each((i, el) => {
+    let teams = $(".team", el)
+    teams = {
+      local: $(teams[0]).text().trim(),
+      visitor: $(teams[1]).text().trim(),
+    }
+  })
+
+  const squad = []
 }
 
 module.exports = getTeam
